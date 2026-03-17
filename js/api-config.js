0ee3682264cafe4a9572e4c62ffe7869bd502641
preedapornpraew23-source <preedapornpraew23@gmail.com>
2026-03-17 17:30:46 +0700

    Add files via upload

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwX0Owko2A2DQX0Y-pmB5ryjfWV_WE1-1lfaOEgy7N9C6sqoPOOi4fxpNm4p32oLjKx/exec';
